f6cabb58224b411da139622273ae45ac5fe09ae9
AverillSaladin03 <averillsaladin2003@gmail.com>
2024-08-14 18:33:23 +0700

    Initial Commit

---
+++ Belajar API/Belajar_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Belajar_APIApp.swift
+//  Belajar API
+//
+//  Created by Averill Saladin Atma Setiawan on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Belajar_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Belajar API/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Belajar API
+//
+//  Created by Averill Saladin Atma Setiawan on 14/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
